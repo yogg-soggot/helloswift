e6368c5fcc3516e5a3beea23b130cb8457dbedc6
Nikita Ivanov <nikita.i@reliz.com>
2023-02-06 08:32:38 +0400

    naming

--- Sources/Analytics/Showcase.swift
+++ Sources/Analytics/Showcase.swift
@@ -1,5 +1,5 @@
 @main
-public struct Analytics {
+public struct Showcase {
     public static func main() {
         ConsoleMockApi().event(token: "token1", payload: [EventPayload(param: "param", value: "value")]);
     }
--- Tests/AnalyticsTests/AnalyticsTests.swift
+++ Tests/AnalyticsTests/AnalyticsTests.swift
@@ -6,6 +6,6 @@ final class AnalyticsTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Analytics().text, "Hello, World!")
+        XCTAssertEqual(Showcase().text, "Hello, World!")
     }
 }
