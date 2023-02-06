4c9f161adcb200ff17f1fa2a2e319b5f11ef2ea7
Nikita Ivanov <nikita.i@reliz.com>
2023-02-06 09:05:47 +0400

    Token key

---
+++ Sources/Analytics/TokenParser/TokenKey.swift
@@ -0,0 +1,15 @@
+//
+// Created by Avein on 06.02.2023.
+//
+
+import Foundation
+
+enum TokenKey { // Assuming this is code gen from excel or something like that
+    case onboarding, home, feed, profile
+    case image, button, text, video
+    case click, tap, scroll
+}
+
+enum Payload: String {
+    case time, user
+}
